8bd285090e6ca4bb812472b2e1ce1dd408fdca19
podamone <papipoda99@gmail.com>
2024-09-21 18:43:14 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
+API_ID = int(getenv("API_ID", 28105090))
+API_HASH = getenv("API_HASH", "4de697ff186d365497af2de4ce8fb3ed)
+OWNER_ID = int(getenv("OWNER_ID", "6020215097"))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/+0PKc9tIYC1hjN2Nl")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "https://t.me/+0PKc9tIYC1hjN2Nl")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "bugadam")
 
 # Random Start Images
 IMG = [
